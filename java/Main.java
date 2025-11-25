69190d8b8d208b7bb511ef89189fa52af4018cd8
LuxZen-Tao <22331820@bucks.ac.uk>
2025-11-25 02:50:05 +0000

    moreFiles

---
+++ java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Dragon draco = new Dragon();   // user will be prompted for stats
+
+        draco.status();
+
+    }
+}
---
+++ java/Untitled-1.java
@@ -0,0 +1,17 @@
+public class TuckShop {
+    private String name;
+    private String location;
+
+    public TuckShop(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+}
